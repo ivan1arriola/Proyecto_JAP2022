2115f4c2da25a32f89b7312fe25a91c4f01a339e
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-08-18 15:01:05 -0300

    Ahora tiene login no funcional

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const HOME = "home.html"
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.getElementById("ingBtn").addEventListener("click", function() {
+        window.location = "home.html"
+    });
+});
