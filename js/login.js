a055a024d29a44cdde71e5909156e5598076e09b
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-08-29 19:59:19 -0300

    elimine boton google

--- js/login.js
+++ js/login.js
@@ -11,14 +11,6 @@ const isComplete = () => {
     return !isEmpty;
 };
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  }
-
 const showAlertError = () => {
     alert(ERROR)
 };
