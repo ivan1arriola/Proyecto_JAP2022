b43c6375e3daa4fa056d9a392f381d158188e630
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-16 15:44:01 -0300

    google login

--- js/login.js
+++ js/login.js
@@ -27,5 +27,18 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     });
 
+    function inicioSesionGoogle(response) {
+        // decodeJwtResponse() is a custom function defined by you
+        // to decode the credential response.
+        const responsePayload = decodeJwtResponse(response.credential);
+   
+        console.log("ID: " + responsePayload.sub);
+        console.log('Full Name: ' + responsePayload.name);
+        console.log('Given Name: ' + responsePayload.given_name);
+        console.log('Family Name: ' + responsePayload.family_name);
+        console.log("Image URL: " + responsePayload.picture);
+        console.log("Email: " + responsePayload.email);
+     }
+
     
 });
