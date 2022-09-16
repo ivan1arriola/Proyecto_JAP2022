f7046260251501466ed3b8d59c780af8ffd58391
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-16 16:37:09 -0300

    agrege  nuevos casos de login error

--- js/init.js
+++ js/init.js
@@ -11,7 +11,7 @@ const USER = localStorage.getItem('user');
 const LOGIN = "login.html"
 
 document.addEventListener("DOMContentLoaded", function(){
-    if(USER==null || USER == "null"){
+    if(USER==null || USER == "null" || USER=="undefined" || USER==undefined){
         window.location = LOGIN;
     }else{
         document.getElementById("user").innerHTML= USER;
