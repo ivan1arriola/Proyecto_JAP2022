7a6bbd3a0b3d6f2390864b8b2b8df3197aeb60c7
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-17 00:26:58 -0300

    Google Oauth funcionando

--- js/login.js
+++ js/login.js
@@ -21,16 +21,14 @@ const loginIn = (user) => {
 
 
 globalThis.inicioSesionGoogle = (response) => {
-    console.log(response);
     const responsePayload = decodeJwtResponse(response.credential);
-    console.log (responsePayload)
 
-    console.log("ID: " + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
-    console.log("Image URL: " + responsePayload.picture);
-    console.log("Email: " + responsePayload.email);
+    //console.log("ID: " + responsePayload.sub);
+    //console.log('Full Name: ' + responsePayload.name);
+    //console.log('Given Name: ' + responsePayload.given_name);
+    //console.log('Family Name: ' + responsePayload.family_name);
+    //console.log("Image URL: " + responsePayload.picture);
+    //console.log("Email: " + responsePayload.email);
 
     localStorage.setItem('user', responsePayload.email);
     loginIn(responsePayload.email)
