1a7996cedd383a218a984e69d9bcf7972c506ad0
Iván Arriola <ivan.arriola@fing.edu.uy>
2022-09-19 19:15:27 -0300

    se agrego carrusel, productos relacionados y mensaje de no hay comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -2,12 +2,20 @@ const prodID = localStorage.getItem("prodID");
 const INFO_URL = PRODUCT_INFO_URL + prodID + EXT_TYPE;
 const COMMENTS_URL = PRODUCT_INFO_COMMENTS_URL + prodID + EXT_TYPE;
 
+const hideCommentsEmpty = () => {
+  document.getElementById('commentsEmpty').style.display = 'none'
+};
+
+const setProdID = (id) => {
+  localStorage.setItem("prodID", id);
+  window.location = "product-info.html"
+}
 
 const commentToHtml = (comment) => {
   const { dateTime: date, description, score, user } = comment;
   return `
     <div class="list-group-item ">
-      <h5><b class=${(score>3)? 'success' : (score<3)? 'danger' : 'warning'} >${user}</b> - ${date} - ${stars(score)}</h5>
+      <h5><b class=${(score > 3) ? 'success' : (score < 3) ? 'danger' : 'warning'} >${user}</b> - ${date} - ${stars(score)}</h5>
       <p>${description}</p>
     </div>
   `;
@@ -17,7 +25,7 @@ const setCommentDate = () => {
   const date = new Date();
   console.log(date)
 
-  const month = (date.getMonth()+1) < 10 ? "0" + (date.getMonth()+1) : (date.getMonth()+1);
+  const month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
   const day = date.getDate() < 9 ? "0" + date.getDate() : date.getDate();
   const year = date.getFullYear();
   const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
@@ -26,14 +34,14 @@ const setCommentDate = () => {
   const seconds =
     date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
 
-    console.log(date.getDate())
+  console.log(date.getDate())
 
   return (
     year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds
   );
 };
 
-const submitScore = () => {
+const submitComment = () => {
   const newComment = {
     product: prodID,
     score: document.getElementById("score").value,
@@ -46,23 +54,58 @@ const submitScore = () => {
   document.getElementById("comment").value = ''
 
   let commentsDiv = document.getElementById("comments");
+  hideCommentsEmpty()
   commentsDiv.innerHTML = commentToHtml(newComment) + commentsDiv.innerHTML;
 };
 
 const showImages = (images) => {
-  imagesDiv = document.getElementById("images");
   let imagesToAppend = "";
+  let carouselIndicators = '';
   images.forEach((image, index) => {
-    imagesToAppend += `
-        <div class="image">
-            <img src="${image}" alt="${"image" + index}" class="img-thumbnail">
-        </div>`;
+
+    if (index == 0) {
+      carouselIndicators += `<li data-target="#carouselIndicators" data-slide-to="${index}" class="active"></li>`;
+      imagesToAppend += `
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="${image}" alt="${"image" + index}">
+      </div>
+          `
+    }
+    else {
+      carouselIndicators += `<li data-target="#carouselIndicators" data-slide-to="${index}"></li>`;
+      imagesToAppend += `
+    <div class="carousel-item ">
+      <img class="d-block w-100" src="${image}" alt="${"image" + index}">
+    </div>
+        `
+    };
   });
-  imagesDiv.innerHTML = imagesToAppend;
+  document.getElementById("images").innerHTML = imagesToAppend;
+  document.getElementById('carousel-indicators').innerHTML = carouselIndicators;
 };
 
+const showRelatedProducts= (products) => {
+  let reletedHTML = ``;
+  products.forEach((product)=>{
+    console.log(product);
+    const {id, name, image} = product;
+    reletedHTML += `
+    <div class="col">
+    <div onclick="setProdID(${id})" class="card" style="width: 18rem;">
+      <img class="card-img-top" src=${image} alt="${name + ' ' + id}">
+      <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+      </div>
+    </div>
+    </div>
+    `
+  })
+  document.getElementById('relatedProducts').innerHTML= reletedHTML;
+
+}
+
 const showInfo = (data) => {
-  const { name, currency, cost, description, category, soldCount, images } =data;
+  const { name, currency, cost, description, category, soldCount, images, relatedProducts } = data;
 
   document.getElementById("product-name").innerHTML = name;
   document.getElementById("precio").innerHTML = currency + " " + cost;
@@ -71,7 +114,7 @@ const showInfo = (data) => {
   document.getElementById("cant-vendidos").innerHTML = soldCount;
 
   showImages(images);
-  // showRelatedProducts(data.relatedProducts)
+  showRelatedProducts(relatedProducts)
 };
 
 const stars = (score) => {
@@ -93,7 +136,7 @@ const stars = (score) => {
 const showComments = (comments) => {
   const commentsDiv = document.getElementById("comments");
   let htmlContentToAppend = "";
-
+  if (comments.length >0) hideCommentsEmpty();
   comments.forEach((comment) => {
     htmlContentToAppend += commentToHtml(comment);
   });
