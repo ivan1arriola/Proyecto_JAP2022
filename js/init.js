df5beb5abb2dab973fa38f56196c0808c54e08dd
Iván Arriola <83203492+ivan1arriola@users.noreply.github.com>
2023-11-17 23:38:31 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 //const host = "https://japceibal.github.io/emercado-api/";
-const host = "https://proyectojap-90e389a055e0.herokuapp.com/";
+const host = "http://floreria.ivan1arriola.me:9128/";
 
 const CATEGORIES_URL = host + "cats";
 const PUBLISH_PRODUCT_URL = host + "sell";
